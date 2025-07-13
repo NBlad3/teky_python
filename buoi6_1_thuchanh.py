134663fe6e9db3ece58a7617ea14db72b8e17e52
NBlad3 <NBFriend123@outlook.com>
2025-07-13 10:22:36 +0200

    added code to buoi6_1_thuchanh.py

--- buoi6_1_thuchanh.py
+++ buoi6_1_thuchanh.py
@@ -1,17 +1,34 @@
 import datetime
 
+
 class Student:
-    def __init__(self, last, birth, code):
+    
+    def __init__(self, name, code, birth, gpa=0.0):
         if birth < int(datetime.datetime.now().strftime("%Y")) - 100 or birth > int(datetime.datetime.now().strftime("%Y")):
             raise Exception("Invalid birthdate")
-        self.last = last
+        self.name = name
         self.birth = birth
         self.code = code
-    def __str__(self):
-        return f"Last Name: {self.last}\nBirthdate: {self.birth}\nStudent code: {self.code}\n"
+        self.gpa = gpa
     
-s1 = Student("Pham", 2000, 123)
-s2 = Student("Nguyen", 2000, 321)
+    def update_score(self, new_gpa):
+        self.gpa = new_gpa
+    
+    def check_academic_performance(self):
+        if self.gpa >= 8.0:
+            print("Academic Performance: Excellent")
+        elif self.gpa >= 6.5:
+            print("Academic Performance: Good")
+        elif self.gpa >= 5.0:
+            print("Academic Performance: Average")
+        else:
+            print("Academic Performance: Poor")
+    
+    def __str__(self):
+        return f"Name: {self.name}\nCode: {self.code}\nBirthdate: {self.birth}\nAverage Score: {self.gpa}"
 
+
+s1 = Student("Nguyen", 123, 2000)
+s1.update_score(5.5)
+s1.check_academic_performance()
 print(s1)
-print(s2)
